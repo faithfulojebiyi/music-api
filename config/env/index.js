9c547165351d0cc4b741744fcc77969db6e83b98
Faithful Ojebiyi <faithfulojebiyi@gmail.com>
2021-08-26 04:04:31 +0100

    Feature: (Set up Config)

---
+++ config/env/index.js
@@ -0,0 +1,31 @@
+import rootPath from 'app-root-path';
+import production from './production'
+import development from './development';
+import test from './test';
+
+const {
+  MYMUSIC_PORT: PORT,
+  MYMUSIC_SECRET: SECRET,
+  MYMUSIC_NODE_ENV: NODE_ENV,
+  MYMUSIC_AWS_ACCESS_KEY: AWS_ACCESS_KEY,
+  MYMUSIC_AWS_SECRET_KEY: AWS_SECRET_KEY,
+  MYMUSIC_BUCKET: AWS_BUCKET_NAME
+} = process.env;
+
+const currentEnv = {
+  production,
+  development,
+  test
+}[NODE_ENV || 'development'];
+
+export default {
+  ...process.env,
+  ...currentEnv,
+  rootPath,
+  PORT,
+  SECRET,
+  NODE_ENV,
+  AWS_ACCESS_KEY,
+  AWS_SECRET_KEY,
+  AWS_BUCKET_NAME
+};
---
+++ config/env/production.js
@@ -0,0 +1,5 @@
+import 'dotenv/config'
+
+export default {
+  DATABASE_URL: process.env.MYMUSIC_POSTGRES_DEV_URL
+}
